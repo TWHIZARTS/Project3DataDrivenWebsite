80b8c322e30f4a0505a030b22a1fc71011df3b63
Tori Whisenant <twhiznt@gmail.com>
2026-01-21 14:36:26 -0500

    Start of Project 3. Data fetched.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from "react"
 function App() {
+
+useEffect(() =>{
+    async function fetchData()
+    {
+
+    const url = "/BoyleSites.json";
+    const response = await fetch(url);
+
+    if (response.ok) 
+      {
+      const result = await response.json();
+      console.log(result);
+      }
+    }
+    fetchData()
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +34,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+     
     </div>
   );
 }
