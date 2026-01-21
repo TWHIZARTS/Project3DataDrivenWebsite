3baeac1802416da6ed7da16c17607bdd502ecf3e
Tori Whisenant <twhiznt@gmail.com>
2026-01-21 14:43:38 -0500

    Milestone 1: Load from JSON file, print out how many records are stored in the file.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect} from "react"
+import { useEffect, useState} from "react";
 function App() {
+  const [ data, setData ]= useState([]);
 
 useEffect(() =>{
+
     async function fetchData()
     {
-
     const url = "/BoyleSites.json";
     const response = await fetch(url);
 
     if (response.ok) 
       {
-      const result = await response.json();
-      console.log(result);
+        const result = await response.json();
+        console.log(result);
+        setData(result);
       }
     }
     fetchData()
   },[]);
+
+  
 
   return (
     <div className="App">
@@ -35,6 +39,9 @@ useEffect(() =>{
           Learn React
         </a>
       </header> */}
+      <p><strong>        
+        Number of records stored in the file: {data.length}
+      </strong></p>
      
     </div>
   );
