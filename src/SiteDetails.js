08c77e97f7d2d7cfa4fa25f488bb84be332ceb2b
Tori Whisenant <twhiznt@gmail.com>
2026-01-22 12:19:14 -0500

    Added IMG and desc. for each site.

--- src/SiteDetails.js
+++ src/SiteDetails.js
@@ -1,28 +1,38 @@
 import react from "react";
 import { useParams } from "react-router-dom";
-
 function SiteDetails(props)
 {
 
     const {SiteNum}= useParams()
     const ID = props.data.find(s => s.SiteID == SiteNum);
-
-    // console.log(ID, SiteNum)
+    console.log(ID, SiteNum)
     
     if (ID===undefined)
     {
       return (
-      <> 
+      <>
+
       <p>Site with ID {ID.SiteID} not found.</p>
       
       </>);
     }
 
     else{
+        console.log(ID.Image)
     return(
-        <>
+        <body>
             <h2>{ID.Site}</h2>
-        </>
+
+            <section>
+                <img src = {`/images/${ID.Image}`} alt = "pic"/>
+            </section>
+
+            
+            <section>
+                {ID.Description}
+            </section>
+            
+        </body>
     );
 }
 }
